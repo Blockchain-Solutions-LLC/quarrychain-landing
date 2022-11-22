2ac4e6b0054f21dd7700da0dc8db874f0c6ff940
daveaneo <daveaneo@gmail.com>
2022-11-22 11:45:41 -0700

    updated FAQ

--- src/app/containers/FAQ/index.tsx
+++ src/app/containers/FAQ/index.tsx
@@ -9,9 +9,26 @@ export function FAQ() {
     <Content>
       <ContentTitle>What is QuarryChain?</ContentTitle>
       <ContentParagraph>
-        QuarryChain is an Advanced Decentralized Blockchain Platform that is
-        dedicated to the establishment of a truly decentralized blockchain.
+        QuarryChain is an advanced blockchain network and ecosystem of tools that enable developers to create and deploy smart contracts and decentralized applications (DApps), freely own, and store data and other content, at high speed, low cost, with minimal environmental impact, and with greater decentralization and governance over current blockchains.
       </ContentParagraph>
+
+      <ContentTitle>How did QuarryChain begin?</ContentTitle>
+      <ContentParagraph>
+        QuarryChain was originally developed as a Proof of Work (PoW) blockchain network back in the summer of 2021. The main idea behind the name was based on the concept of “CryptoCurrency Mining”, where you have a metaphorical term that describes users competing against each other with specialized computing hardware to solve complex mathematical puzzles to get in return a block reward. This usually takes a lot of effort and time, therefore miners (block validators) will get together and pool their resources to form “Mining Pools” to lower costs and of course increase their power and chances of mining future blocks.
+        <br/> <br/>
+        However, in real life when people such as contractors/miners/companies come together and pool their resources to collectively mine something, we do not call these places “Mining Pools” but instead we call them “Quarries”. Blockchain can therefore be easily thought out as a “Digital Quarry”, where users are actively “mining” blocks and coming together through industries throughout the entire world.
+      </ContentParagraph>
+
+      <ContentTitle>What makes QuarryChain “Advanced”?</ContentTitle>
+      <ContentParagraph>
+        The QuarryChain Network uses an innovative Delegated Proof of Stake consensus mechanism in which 27 Senior Representatives (SRs) produce blocks for the network. Every 6 hours, QuarryChain Token account holders who freeze their accounts can vote for a selection of SR candidates, with the top 27 candidates deemed the SRs. Voters may choose SRs based on criteria such as projects sponsored by SRs to increase QuarryChain's Token adoptions, and rewards distributed to voters. This allows for a more democratized and decentralized ecosystem. SRs' accounts are normal accounts, but their accumulation of votes allows them to produce blocks. QuarryChain's DPoS system offers an innovative mechanism resulting in 20,000 - 100,000 TPS (Transactions per second)  compared to Bitcoin's 3 TPS and Ethereum's 15 TPS.
+      </ContentParagraph>
+
+      <ContentTitle>What is the Governance like on QuarryChain?</ContentTitle>
+      <ContentParagraph>
+        The Community (Stakeholders) need to have the ability to elect those with specific skills, public credibility, and expertise to fill the roles that are lacking in the system. Every account in the QuarryChain Network can apply and have the opportunity to become a Senior Representative ( denoted as SR). Everyone can vote for SR candidates. The top 27 candidates with the most votes will become SRs with the right and obligation to generate blocks. The votes are counted every 6 hours and the SRs will change accordingly.
+      </ContentParagraph>
+
       <ContentTitle>What is the QuarryChain Protocol?</ContentTitle>
       <ContentParagraph>
         The QuarryChain Protocol, aims to be one of the largest blockchain-based
@@ -20,17 +37,27 @@ export function FAQ() {
         high availability for all Decentralized Applications(DApps) in the
         QuarryChain Ecosystem.
       </ContentParagraph>
+
       <ContentTitle>What is QuarrySwap?</ContentTitle>
       <ContentParagraph>
-        QuarrySwap is a decentralized exchange (DEX, for short), and one of
-        QuarryChain's first Applications. QuarrySwap also happens to be
-        non-custodial, meaning that unlike centralized exchanges, Quarryswap
-        does not need to possess your tokens in order for you to be able to
-        trade them. Instead, QuarrySwap allows users to trade trustfully,
-        peer-to-peer, with liquidity that is supplied by other users. This means
-        that new projects can easily connect to their desired markets as long as
-        some entity is willing to provide the liquidity.
+        QuarrySwap is a decentralized exchange (DEX, for short), and one of QuarryChain's first Applications. QuarrySwap also happens to be non-custodial, meaning that unlike centralized exchanges, Quarryswap does not need to possess your tokens in order for you to be able to trade them. Instead, QuarrySwap allows users to trade trustfully, peer-to-peer, with liquidity that is supplied by other users. This means that new projects can easily connect to their desired markets as long as some entity is willing to provide the liquidity.
       </ContentParagraph>
+
+      <ContentTitle>What is in the QuarryChain Ecosystem?</ContentTitle>
+      <ContentParagraph>
+        QuarryChain is a robust blockchain ecosystem designed and developed by the QuarryChain development team, which follows QuarryChain’s Philosophy ``Get quality with quarry”. There are multiple kinds of products involved in QuarryChain’s Ecosystem, including public chain, wallet client, decentralized exchange, and its decentralized applications (DApps). QuarryChain aims to bring a Real World Marketplace to blockchain technology, this will all be done through QuarryChain’s Tokenization of Real World Assets.
+      </ContentParagraph>
+
+      <ContentTitle>How will QuarryChain Tokenize Real World Assets?</ContentTitle>
+      <ContentParagraph>
+        QuarryChain enables tokenization of assets in a variety of industries to create vast liquidity across the whole global assets market. QuarrySwap can turn almost any asset, real or virtual, into a digital token to be owned, traded, transferred, or stored without the use of a central third-party or intermediary. This will be all done through QuarryChain’s QRY-20 Smart Contracts, which will power QuarrySwap’s tokenization features and associated digital assets.
+      </ContentParagraph>
+
+      <ContentTitle>What are User’s and Use Cases of QuarryChain?</ContentTitle>
+      <ContentParagraph>
+        QuarryChain creates outstanding value and benefits for a wide variety of user groups and industries world wide. From Finance, Developers, Business Owners & Entrepreneurs, Real Estate, Precious Metals, Collectibles, Health, Environment, Manufacturing, and Intangible Assets. All of which will be powered and provided by the QuarryChain Network.
+      </ContentParagraph>
+
       <ContentTitle>What is QuarryChain's Consensus Mechanism?</ContentTitle>
       <ContentParagraph>
         The QuarryChain consensus mechanism uses an innovative Delegated Proof
